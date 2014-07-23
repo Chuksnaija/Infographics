b5871ecf686d4be70f7d656639b5d2ced56faa15
Chuksnaija <c.ojidoh@gmail.com>
2014-07-23 18:03:57 +0200

    Update control.js

--- treemap/js/control.js
+++ treemap/js/control.js
@@ -11,15 +11,15 @@ $( document ).ready(function() {
 		// NOTE: Your column titles cannot contain spaces.
 		var nested_data = d3.nest()
 			.key(function(d)  { return d.Destination; })
-			.key(function(d)  { return d.Ticket_class_Destination; })
 			.key(function(d)  { return d.Supplier_name; })
+			.key(function(d)  { return d.Ticket_class_description; })
 			.entries(csv_data);
 
 		// Creat the root node for the treemap
 		var root = {};
 
 		// Add the data to the tree. This gives us a new top level "god" node. A parent of everything. 
-		root.key = "Data";
+		root.key = "data";
 		root.values = nested_data;
 
 		// Change the key names and children values from .next and add values for a chosen column to define the size of the blocks, e.g. a value
